51379302602e0d546f0a0ee026dc954ab8cdfe62
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-22 12:17:39 +0200

    var tests

--- hello.go
+++ hello.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 )
 
+const PI = 3.14;
+
 var name = "Mario";
 var name2 string = "Luigi";
+var a, b = 1, "peach";
 
 // main function
 func main() {
 	fmt.Println("hello world");
 	fmt.Println(name);
 	fmt.Println(name2);
+	fmt.Println(a, b);
+	fmt.Println("PI: ", pi)
 }
